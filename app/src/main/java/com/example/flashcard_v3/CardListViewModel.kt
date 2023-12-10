f7ff71ba000085e800fe8dc7cfa4778cab6403d6
Mert Tüfekçi <mert4x4@gmail.com>
2023-12-10 17:55:37 +0300

    son

--- app/src/main/java/com/example/flashcard_v3/CardListViewModel.kt
+++ app/src/main/java/com/example/flashcard_v3/CardListViewModel.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.flashcard_v3.Card
 
 
 class CardListViewModel : ViewModel() {
